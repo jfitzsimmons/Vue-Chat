480623e89363820ae9bcc6ed27b61cc9a5ec8ee7
jfitzsimmons <jfitzhome@gmail.com>
2019-08-27 13:35:38 -0500

    working demo

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,36 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div>
+    <Initialize />
+    <ConversationContainer
+      v-for="id in convoIds"
+      :conversation="conversations[id]"
+      :id="id"
+      :key="id"
+    />
   </div>
 </template>
 
 <script>
+import Initialize from './Initialize.vue'
+import ConversationContainer from './ConversationContainer.vue'
+
+import { mapState } from 'vuex'
+
 export default {
   name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+  components: {
+    Initialize,
+    ConversationContainer
+  },
+
+  computed: {
+    ...mapState({
+      conversations: state => state.conversations.all,
+      convoIds: state => state.conversations.allIds
+    })
+  },
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style>
 </style>
---
+++ src/config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: "AIzaSyAw25NEs-FrtnFwhWOJrdKk14HP9NUlLFQ",
+  authDomain: "vue-mvp-f835d.firebaseapp.com",
+  databaseURL: "https://vue-mvp-f835d.firebaseio.com",
+  projectId: "vue-mvp-f835d",
+  storageBucket: "vue-mvp-f835d.appspot.com",
+  messagingSenderId: "667523448349"
+};
+
+export default config
